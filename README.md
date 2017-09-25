a7c9401059e315e71d05b8aa6a169eec8c615609
Sam Sheehan <samsheehan@Sams-MacBook-Pro.local>
2017-09-25 16:46:59 -0500

    Created a class framework for the Tamagotchi

--- README.md
+++ README.md
@@ -1,1 +1,35 @@
 # sheehan-tamagotchi-miniproject
+
+## Tamagotchi
+
+## Description
+
+Let's use our front-end web development skills to create a "living" pet! We'll use HTML, CSS, and Javascript to interact with our pet.
+
+## Requirements
+
+- Create a repo for your tamagotchi pet.
+- Make a commit after you finish each one of the following:
+
+1. Create a class for you tamagotchi.
+2. Display a character of your choice on the screen to represent your pet.
+3. Display the following metrics for your pet:
+	- Hunger (1-10 scale)
+	- Sleepiness (1-10 scale)
+	- Boredom (1-10 scale)
+	- Age
+4. Add buttons to the screen to feed your pet, turn off the lights, and play with your pet.
+5. Add the ability to name your pet.
+6. Style the page.
+7. Increase your pet's age every x minutes.
+8. Increase your pet's Hunger, Sleepiness, and Bored metrics on an interval of your choosing.
+9. You pet should die if Hunger, Boredom, or Sleepiness hits 10.
+10. Morph your pet at certain ages.
+11. Animate your pet across the screen.
+
+## Extras
+
+- Have your tamagotchi give birth to baby tamagotchi.
+- With special powers (extends the class).
+- Add an excercise method to your tamagotchi that affects certain properties.
+- Add anything you can think of, use your imagination.
--- scripts/app.js
+++ scripts/app.js
@@ -1,6 +1,82 @@
-console.log("Running JS file");
+console.log("Running Tamagotchi JS File");
 
+console.log("--------------------------");
 
+// For more info about Tamagotchi, go to this link: http://tamagotchi.wikia.com/wiki/Tamagotchi
 
+class Tamagotchi {
+	constructor(aliveStatus){
+		this.name = "None";
+		this.hunger = 1; // 1-10 scale; 1 = least hungry, 10 = starved
+		this.sleepiness = 1; // 1-10 scale; 1 = most awake, 10 = "final sleep"
+		this.boredom = 1; // 1-10 scale; 1 = excited, 10 = bored to death
+		this.age = 0;
+		this.alive = aliveStatus;
+		this.lightsOn = true;
+		this.currentForm = "Stage 1";
+		this.potentialForms = ["Stage 1", "Stage 2", "Stage 3", "Stage 4", "Stage 5"];
+	}
 
-console.log("Finished running JS file");
+	feed(){
+		// code
+	}
+
+	playGame(){
+		// code
+	}
+
+	clean(){
+		// code
+	}
+
+	toggleLights(){
+		// code
+	}
+
+	showHunger(){
+		// code
+	}
+
+	showSleepiness(){
+		// code
+	}
+
+	showBoredom(){
+		// code
+	}
+
+	showAge(){
+		// code
+	}
+
+	namePet(){
+		// code
+	}
+
+	increaseAge(){
+		// code
+	}
+
+	increaseConditionStats(){
+		// code
+	}
+
+	dying(){
+		// code
+	}
+
+	morphPet(){
+		// code
+	}
+
+	animatePet(){
+		// code
+	}
+}
+
+// Driver Code:
+// const newTama = new Tamagotchi(true);
+
+console.log("-----------------------------------");
+
+console.log("Finished Running Tamagotchi JS File");
